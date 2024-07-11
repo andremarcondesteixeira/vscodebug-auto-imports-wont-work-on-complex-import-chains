de65733fae766473f4a9494d0d25f71b58ff6d68
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-07-10 23:46:08 -0300

    initial commit

---
+++ js/custom-elements/assets-list/assets-list-element.js
@@ -0,0 +1,15 @@
+import { ASSET_TYPE_ENUMERATION, ASSET_TYPE_FOLDER, ASSET_TYPE_RECORD } from "../../domain/constants.js";
+
+class AssetsListElement extends HTMLElement {
+    // listItemBuilders = {
+    //     [ASSET_TYPE_ENUMERATION]: () => {},
+    //     [ASSET_TYPE_FOLDER]: () => {},
+    //     [ASSET_TYPE_RECORD]: () => {},
+    // };
+
+    constructor() {
+        super();
+    }
+}
+
+customElements.define('assets-list', AssetsListElement);
---
+++ js/domain/constants.js
@@ -0,0 +1,3 @@
+export const ASSET_TYPE_ENUMERATION = "enumeration";
+export const ASSET_TYPE_FOLDER = "folder";
+export const ASSET_TYPE_RECORD = "record";
